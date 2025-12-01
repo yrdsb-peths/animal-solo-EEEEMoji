a5a30a95b7c077c46e8588b1c189d8facff451f8
EEEEMoji <ethanyeungsikyuen@gmail.com>
2025-12-01 11:47:48 -0500

    P2

--- MyWorld.java
+++ MyWorld.java
@@ -1,11 +1,32 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
+        
+        Label scoreLabel = new Label(0,80);
+        addObject(scoreLabel, 20, 20); 
         createApple();
+    }
+    
+    /*
+     * Endthe game and draw 'GameOver'
+     */
+    public void gameOver(){
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    /**
+     * Increase score
+     */
+    public void increaseScore(){
+        score++;
+        scoreLabel. setValue(score);
     }
     
     public void createApple(){
